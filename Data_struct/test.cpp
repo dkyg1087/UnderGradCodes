ebc3318c015808936faadf0903e34079ffcb20f0
dkyg1087 <43636582+dkyg1087@users.noreply.github.com>
2019-12-07 23:20:39 +0800

    Basically did a test

--- Data_struct/test.cpp
+++ Data_struct/test.cpp
@@ -1,11 +1,5 @@
 #include<iostream>
 using namespace std;
 int main(){
-    string str;
-    while(getline(cin,str),str!="0"){
-        for(int i=0;str.size();i++){
-            cout<<str[i];
-        }
-        cout<<endl;
-    }
+    cout<<"Hello world!"<<endl;
 }
