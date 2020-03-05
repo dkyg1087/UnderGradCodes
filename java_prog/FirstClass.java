082c9d0f750dabf93cf80cdb72ef0cc6ff4a7cb4
dkyg1087 <43636582+dkyg1087@users.noreply.github.com>
2020-03-05 21:32:46 +0800

    new sem

---
+++ java_prog/FirstClass.java
@@ -0,0 +1,8 @@
+// 這是你這堂課的第一支Java程式
+public class FirstClass{
+    // main method begins execution of Java application
+    public static void main( String args[] ){
+        System.out.println( "這是我的第一支Java程式!\n學號：\"00757143\" \n姓名：\"楊明哲\"");
+        System.out.println( "" );
+    } // end method main
+} // end class FirstClass
