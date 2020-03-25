48f03db8d9389654c51969d21a887e90c1de7994
dkyg1087 <43636582+dkyg1087@users.noreply.github.com>
2020-03-25 13:20:46 +0800

    dsad

--- C++_prog/test.cpp
+++ C++_prog/test.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 using namespace std;
-#define SIZE 8
 int main(){
-	int arr[SIZE];
+	
 }
--- Data_struct/test.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-int main(){
-    int num1=stoi("23sdada21312");
-    cout<<num1;
-}
