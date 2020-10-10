a119b6e2529c9e54d1102dbba59eb75f691eefa4
Derek Yang <43636582+dkyg1087@users.noreply.github.com>
2020-10-11 00:06:34 +0800

    add graph and com_graph

---
+++ Graph_Algorithm/longest_Inc_subs.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int lis(vector<int> const& a) {
+    int n = a.size();
+    const int INF = 1e9;
+    vector<int> d(n+1, INF);
+    d[0] = -INF;
+
+    for (int i = 0; i < n; i++) {
+        int j = upper_bound(d.begin(), d.end(), a[i]) - d.begin();
+        if (d[j-1] < a[i] && a[i] < d[j])
+            d[j] = a[i];
+    }
+
+    int ans = 0;
+    for (int i = 0; i <= n; i++) {
+        if (d[i] < INF)
+            ans = i;
+    }
+    return ans;
+}
+int main(){
+    int amount=0;
+    vector<int>intSeq;
+    cin>>amount;
+    while(amount--){
+        int num=0;
+        cin>>num;
+        intSeq.push_back(num);
+    }
+    cout<<lis(intSeq)<<endl;
+}
